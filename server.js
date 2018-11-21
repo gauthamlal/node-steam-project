65b994964f67f49681b4d44fea9c62288767fba8
Gautham Lal <gautham1995@gmail.com>
2018-11-22 00:03:55 +0530

    Added News API

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const request = require('request');
 
 const {API_KEY} = require('./keys/keys.js') || 'ENTER_YOUR_API_KEY';
 let url = 'http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=292030&format=json';
+let urlNews = 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=292030&count=4&maxlength=300&format=json';
 
 const app = express();
 
 app.get('/',(req, res) => {
   request(url, (error, response, body) => {
-    res.send(body);
+    res.send(response);
+  });
+});
+
+app.get('/news', (req, res) => {
+  request(urlNews, (error, response, body) => {
+    let reqBody = body.toString();
+    reqBody = JSON.parse(reqBody);
+    res.send(reqBody);
   });
 });
 
